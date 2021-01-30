8cd27cae5e755a9838ff2a237a320775f548bd8f
Lukecp <lukecp@pm.me>
2021-01-30 00:08:31 -0500

    Redux components in place. Action building in progress

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+export const addNewFeature = (feature) => {
+	return {
+		type: 'ADD_ITEM', payload: feature
+	}
+}
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { addNewFeature } from '../actions';
 
 const AdditionalFeature = props => {
+  console.log(props)
+  
+  const addFeatureHandler = (e) => {
+    e.preventDefault();
+    props.addNewFeature(props.feature)
+  };
+  
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={addFeatureHandler}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+const mapPropsToState = (state) => {
+  return {
+
+  }
+}
+
+export default connect(mapPropsToState, { addNewFeature })(AdditionalFeature);
